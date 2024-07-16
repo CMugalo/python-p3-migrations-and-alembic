9ba1a4ded93c02fbd534b3e96ac528e973651e9f
Christian Mugalo <christian.mugalo@student.moringaschool.com>
2024-07-16 10:00:52 +0300

    migrations and alembic

---
+++ app/migrations/versions/5504f7817baa_empty_init.py
@@ -0,0 +1,24 @@
+"""Empty Init
+
+Revision ID: 5504f7817baa
+Revises: 
+Create Date: 2024-07-16 09:24:24.587394
+
+"""
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision = '5504f7817baa'
+down_revision = None
+branch_labels = None
+depends_on = None
+
+
+def upgrade() -> None:
+    pass
+
+
+def downgrade() -> None:
+    pass
